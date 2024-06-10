23cf87000c5914e63eb3c3c85a3b08034d00c496
Nitya Verma <vrmnitya@gmail.com>
2024-06-10 10:26:09 +0530

    about and home page

--- Session_1/app.py
+++ Session_1/app.py
@@ -3,9 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route("/home")   #for the same server we can have multiple routes
 def index():
-    return "Hello World"
+    return "<H1>Welcome to home page</H1>"
 
+
+@app.route("/about")
+def about():
+    return "<H1>Welcome to about page</H1>"
 
 if __name__ == "__main__":
-    app.run(debug = True, port= 3000)    
+    app.run(debug = True)    
