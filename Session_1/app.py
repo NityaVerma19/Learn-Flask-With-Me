2563487dd255137d92aec28c6538a83425ff87bc
Nitya Verma <vrmnitya@gmail.com>
2024-06-10 11:08:07 +0530

    adding path parameters

--- Session_1/app.py
+++ Session_1/app.py
@@ -8,6 +8,19 @@ def index():
     return "<H1>Welcome to home page</H1>"
 
 
+@app.route("/welcome/<name>")   #PATH parameters
+def welcome(name):
+    return f"<h1>Hi {name} </h1>"  #Note : these parameters (eg. name), by default are strings
+
+@app.route("/addition/<int:num>")  #taking an int path parameter
+def addition(num):
+    return f"Input is {num}, output is {num + 10}"
+
+@app.route("/addition_two/<int:num1>/<int:num2>")  #taking an 2 int path parameter
+def addition_two(num1, num2):
+    return f"{num1} + {num2}, is {num1 + num2}"
+
+
 @app.route("/about")
 def about():
     return "<H1>Welcome to about page</H1>"
